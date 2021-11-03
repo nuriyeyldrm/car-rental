759eaae8d2bc1e0baed03b7ef6f3eb53d2fb96b1
nuriyeyldrm <nuriye.yildirim@std.antalya.edu.tr>
2021-11-03 08:11:55 +0300

    updated

--- src/main/java/com/backend/carrental/repository/UserRepository.java
+++ src/main/java/com/backend/carrental/repository/UserRepository.java
@@ -10,7 +10,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Date;
 import java.util.Optional;
 
 @Repository
--- src/main/java/com/backend/carrental/service/CarService.java
+++ src/main/java/com/backend/carrental/service/CarService.java
@@ -2,22 +2,13 @@ package com.backend.carrental.service;
 
 import com.backend.carrental.domain.Car;
 import com.backend.carrental.domain.FileDB;
-import com.backend.carrental.domain.Role;
-import com.backend.carrental.domain.User;
-import com.backend.carrental.domain.enumeration.UserRole;
 import com.backend.carrental.exception.BadRequestException;
-import com.backend.carrental.exception.ConflictException;
-import com.backend.carrental.exception.ResourceNotFoundException;
 import com.backend.carrental.repository.CarRepository;
 import com.backend.carrental.repository.FileDBRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 @AllArgsConstructor
